8a016bf721edb4fe34f1c38e1aceb1cd2d3ea2a3
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-02-20 14:44:55 +0100

    test

--- api/routes/books/book.ts
+++ api/routes/books/book.ts
@@ -4,7 +4,7 @@ export const getBooks = (request: any, response: any) => {
     pool.query('SELECT * FROM books ORDER BY id ASC', (error, results) => {
         if (error) {
             throw error;
-           }
+        }
         response.status(200).json(results.rows);
     });
 };
