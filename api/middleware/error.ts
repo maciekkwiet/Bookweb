8082e318583776431cf3ad167bed27b6051bd317
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-02-20 15:05:27 +0100

    textX2

--- api/middleware/error.ts
+++ api/middleware/error.ts
@@ -1,4 +1,7 @@
 module.exports = function (err: any, req: any, res: any, next: any) {
     console.log(err);
-    res.status(500).json({ error: 'Something failed' });
+
+
+
+            res.status(500).json({ error: 'Something failed' });
 };
