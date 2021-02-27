8a01f344545b2ba8665a719e99f6ff4115f9d611
wojaneta <wojaneta@gmail.com>
2021-02-27 16:55:42 +0100

    authorsRoutes created

---
+++ api/routes/authors/authorsRoutes.ts
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+import { getAuthors, getAuthorById, createAuthor, updateAuthor, deleteAuthor } from './author';
+
+router.get('/authors', getAuthors);
+router.get('/author:id', getAuthorById);
+router.get('/author/:id/books', getBooksByAuthorId);
+router.post('/author', createAuthor);
+router.put('/author/:id', updateAuthor);
+router.delete('/author/:id', deleteAuthor);
+
+module.exports = router;
