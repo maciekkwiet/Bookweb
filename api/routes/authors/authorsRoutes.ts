36475d3f1be8311866af098fcc1a3dd396e0d717
wojaneta <wojaneta@gmail.com>
2021-02-28 13:16:22 +0100

    getBooksByAuthorId added

--- api/routes/authors/authorsRoutes.ts
+++ api/routes/authors/authorsRoutes.ts
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-import { getAuthors, getAuthorById, createAuthor, updateAuthor, deleteAuthor } from './author';
+import { getAuthors, getAuthorById, getBooksByAuthorId, createAuthor, updateAuthor, deleteAuthor } from './author';
 
 router.get('/authors', getAuthors);
-router.get('/author:id', getAuthorById);
+router.get('/author/:id', getAuthorById);
 router.get('/author/:id/books', getBooksByAuthorId);
 router.post('/author', createAuthor);
 router.put('/author/:id', updateAuthor);
