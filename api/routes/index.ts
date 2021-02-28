ab7d48a0b278f0a7944efa146dab5b2fd43bc3e3
wojaneta <wojaneta@gmail.com>
2021-02-28 13:14:58 +0100

    index.ts code changed

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const bookRoutes = require('./books/bookRoutes');
+const authorsRoutes = require('./authors/authorsRoutes');
 
 router.use('/books', bookRoutes);
+router.use('/authors', authorsRoutes);
 
 module.exports = router;
