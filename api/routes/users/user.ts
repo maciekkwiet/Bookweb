f5e313e4dfbf770567bf5b30acf1be13909eaf14
sebslon <sebastesterprogram@gmail.com>
2021-02-28 13:57:27 +0100

    Add user routes

--- api/routes/users/user.ts
+++ api/routes/users/user.ts
@@ -1,72 +1,58 @@
 import pool from '../../configDB/config';
+const bcrypt = require('bcrypt');
 
 export const getAllUsers = (req: any, res: any) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else {
-      res.status(200).json(results.rows);
-    }
-  });
+  pool
+    .query('SELECT * FROM users ORDER BY id ASC')
+    .then(results => res.status(200).json(results.rows))
+    .catch(error => res.status(500).json(error.message));
 };
 
 export const getUser = (req: any, res: any) => {
-  const id = req.params.id;
+  pool
+    .query('SELECT * FROM users WHERE id = $1', [req.params.id])
+    .then(results => {
+      if (results.rows.length === 0) return res.status(400).send("User with given ID doesn't exist");
 
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      res.status(400).send(error.message);
-    }
-    if (results.rows.length === 0) return res.status(400).send("User with given ID doesn't exist");
-
-    res.status(200).json(results.rows);
-  });
-};
-//id, email, password, name, surname, avatar
-export const addUser = (req: any, res: any) => {
-  const { email, password, name, surname, avatar } = req.body;
-
-  pool.query(
-    'INSERT INTO users (email, password, name, surname, avatar) values ($1, $2, $3, $4, $5)',
-    [email, password, name, surname, avatar],
-    (error, results) => {
-      if (!password || !email) return res.status(400).send(`Email and password are required fields.`);
-      if (error) {
-        res.status(400).json(error.message);
-      } else {
-        res.status(201).json('User Added');
-      }
-    }
-  );
+      res.status(200).json(results.rows);
+    })
+    .catch(error => res.status(500).json(error.message));
 };
 
 export const removeUser = (req: any, res: any) => {
-  const id = req.params.id;
+  pool
+    .query('DELETE FROM users where id = $1', [req.params.id])
+    .then(results => {
+      if (results.rows.length === 0) return res.status(400).send("User with given ID doesn't exist");
 
-  pool.query('DELETE FROM users where id = $1', [id], (error, results) => {
-    if (!id) return res.status(400).send("User with given ID doesn't exist");
-    if (error) return res.status(500).json(error.message);
-
-    res.status(200).json('User deleted');
-  });
+      res.status(200).json('User deleted');
+    })
+    .catch(error => res.status(500).json(error.message));
 };
 
-export const updateUser = (req: any, res: any) => {
-  const id = req.params.id;
+export const updateUser = async (req: any, res: any) => {
+  const { id } = req.params;
   const { email, password, name, surname, avatar } = req.body;
 
-  pool.query(
-    'UPDATE users SET email = $1, password = $2, name = $3, surname = $4, avatar = $5 WHERE id = $6',
-    [email, password, name, surname, avatar, id],
-    (error, results) => {
-      if (error) {
-        res.status(400).json(error.message);
-      } else {
-        res.status(200).json({
-          message: 'User updated',
-          user: results.fields,
-        });
-      }
-    }
-  );
+  await pool
+    .query('SELECT * FROM users WHERE id = $1', [id])
+    .then(results => {
+      if (results.rows.length === 0) return res.status(400).send("User with given ID doesn't exist");
+    })
+    .catch(error => res.status(500).json(error.message));
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  pool
+    .query('UPDATE users SET email = $1, password = $2, name = $3, surname = $4, avatar = $5 WHERE id = $6', [
+      email,
+      hashedPassword,
+      name,
+      surname,
+      avatar,
+      id,
+    ])
+    .then(results => res.status(200).json('User updated..'))
+    .catch(error => res.status(500).json(error.message));
 };
--- api/routes/users/userRoutes.ts
+++ api/routes/users/userRoutes.ts
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-import { getAllUsers, getUser, addUser, removeUser, updateUser } from './user';
+import { getAllUsers, getUser, removeUser, updateUser } from './user';
 
 router.get('/', getAllUsers);
 router.get('/:id', getUser);
-router.post('/', addUser);
 router.delete('/:id', removeUser);
-router.patch('/:id', updateUser);
+router.put('/:id', updateUser);
 
 module.exports = router;
