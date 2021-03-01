809a1118cb006b1edc579d32781c8e469be62c7c
Kamil Dzierzak <kamdzierzak@gmail.com>
2021-03-01 17:04:50 +0100

    Move auth routes to user routes

--- api/routes/user/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { registerValidator, loginValidator } = require('../../middleware/validator');
-import { registerUser, loginUser } from './user';
-
-router.post('/register', registerValidator, registerUser);
-router.post('/login', loginValidator, loginUser);
-
-module.exports = router;
