c0fbab5ab9b771cf0b9987984c2902b813cdfc49
sebslon <sebastesterprogram@gmail.com>
2021-03-03 15:58:55 +0100

    Implement nodemailer to send emails with Gmail

---
+++ api/utils/nodemailerConfig.ts
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+async function sendEmail(receiver: string, subject: string, text: string) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: receiver,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions, function (err: Error, data: {}) {
+    if (err) {
+      console.log('Error: ' + err);
+    } else {
+      console.log('Email sent successfully to: ' + receiver);
+    }
+  });
+}
