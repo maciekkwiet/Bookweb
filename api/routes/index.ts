f57698bd96afee32093a4c9191254262254d2992
wojaneta <wojaneta@gmail.com>
2021-03-03 18:25:01 +0100

    index.ts updated

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const bookRoutes = require('./books/bookRoutes');
 const userRoutes = require('./users/userRoutes');
 const authorsRoutes = require('./authors/authorsRoutes');
+const publishersRoutes = require('./publishers/publishersRoutes');
 
 router.use('/books', bookRoutes);
 router.use('/users', userRoutes);
 router.use('/authors', authorsRoutes);
+router.use('/publishers', publishersRoutes);
 
 module.exports = router;
