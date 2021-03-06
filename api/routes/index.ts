f4bcf1e690aaabf8c23019006801bb7f119ef728
wojaneta <wojaneta@gmail.com>
2021-03-06 12:02:33 +0100

    index.ts fixed

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,13 +1,12 @@
-import { Express } from 'express';
-
+const router = require('express').Router();
 const bookRoutes = require('./books/bookRoutes');
 const userRoutes = require('./users/userRoutes');
 const authorsRoutes = require('./authors/authorsRoutes');
 const publishersRoutes = require('./publishers/publishersRoutes');
 
-module.exports = (app: Express) => {
-  app.use('/api/books', bookRoutes);
-  app.use('/api/users', userRoutes);
-  app.use('/api/authors', authorsRoutes);
-  app.use('/api/publishers', publishersRoutes);
-};
+router.use('/books', bookRoutes);
+router.use('/users', userRoutes);
+router.use('/authors', authorsRoutes);
+router.use('/publishers', publishersRoutes);
+
+module.exports = router;
