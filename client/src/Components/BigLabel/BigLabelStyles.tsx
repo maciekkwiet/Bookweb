a066d9399fdba0f6515cdd2fc8d6fb18c8340f28
Wojtek Warmuz <wwarmuz@gmail.com>
2021-03-12 18:41:06 +0100

    add responsive units

--- client/src/Components/BigLabel/BigLabelStyles.tsx
+++ client/src/Components/BigLabel/BigLabelStyles.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const BigLabelComponent = styled.h2`
   font-family:${(props) => props.theme.fonts.default};
   color: ${(props) => props.theme.colors.primary};
-  height:2em;
-  line-height:2em;
-  font-size: 24px;
+  height:2.5em;
+  line-height:2.5em;
+  font-size: 3vh;
   background: ${(props) => props.theme.backgrounds.lightTransparent};
   box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
   text-align:center;
