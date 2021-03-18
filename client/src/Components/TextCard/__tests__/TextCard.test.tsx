de7c1b73eb8dc72017d21b5af673f79dececd9a4
wojaneta <wojaneta@gmail.com>
2021-03-18 19:47:45 +0100

    Test fixed

--- client/src/Components/TextCard/__tests__/TextCard.test.tsx
+++ client/src/Components/TextCard/__tests__/TextCard.test.tsx
@@ -2,15 +2,13 @@ import { render } from '@testing-library/react';
 import 'jest-styled-components';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../../Theme';
-import { TextCard } from '../TextCard';
+import TextCard from '../TextCard';
 
 describe('TextCard', () => {
   it('render without crash', () => {
-    const mockCallback = jest.fn();
-
     render(
       <ThemeProvider theme={theme}>
-        <TextCard>Tytuł książki/Autor/Dodaj książkę</TextCard>
+        <TextCard>AuthorName AuthorLastName Description</TextCard>
       </ThemeProvider>,
     );
   });
