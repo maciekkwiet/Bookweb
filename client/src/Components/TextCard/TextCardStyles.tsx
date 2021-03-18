848347fbca26a24b7ae5a1bb4480fd2f05db03c4
wojaneta <wojaneta@gmail.com>
2021-03-18 15:50:20 +0100

    Styles changed

--- client/src/Components/TextCard/TextCardStyles.tsx
+++ client/src/Components/TextCard/TextCardStyles.tsx
@@ -1,12 +1,32 @@
 import styled from 'styled-components';
 
-export const TextCardComponent = styled.h2`
-  font-family: ${(props) => props.theme.fonts.default};
-  color: ${(props) => props.theme.colors.primary};
-  height: 2.5em;
-  line-height: ${(props) => props.theme.h2.lineHeight};
-  font-size: ${(props) => props.theme.h2.fontSize};
-  background: ${(props) => props.theme.backgrounds.lightTransparent};
-  box-shadow: ${(props) => props.theme.shadows.inset};
-  text-align: center;
+export const TextCardComponent = styled.div`
+  position: absolute;
+  width: 755px;
+  height: 180px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-color: #000000;
+  border-radius: 10px;
+`;
+
+export const Name = styled.h1`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 16px;
+  text-transform: uppercase;
+  color: #000000;
+`;
+
+export const Description = styled.p`
+  position: absolute;
+  width: 558px;
+  height: 102px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 17px;
+  color: #000000;
 `;
