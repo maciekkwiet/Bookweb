8f79128485a56b9257dcdb3564c201874f0f9e90
wojaneta <wojaneta@gmail.com>
2021-03-20 14:42:04 +0100

    Theme fixed up

--- client/src/Theme.tsx
+++ client/src/Theme.tsx
@@ -22,25 +22,13 @@ export const theme = {
     sentence: `Clicker Script`,
     lightGray: '#505050',
   },
-  backgrounds: {
-    bright: `rgb(242,243,244)`,
-    lightTransparent: `rgba(255,255,255,0.5)`,
-    darkTransparent: `rgba(0,0,0,0.15)`,
-    mainGradient: `linear-gradient(312.01deg, #CB997E 0.66%, #9EB2CD 100%)`,
-    bigCardGradient: `linear-gradient(289.63deg, #B98163 8.13%, rgba(70, 68, 66, 0) 139.61%);`
-  },
-  fonts: {
-    default: `Montserrat`,
-    logo: `Esteban`,
-    sentence: `Clicker Script`
-  },
   h2: {
     fontSize: `3vh`,
     lineHeight: `2.5em`,
   },
   shadows: {
     inset: `inset 0px 4px 4px rgba(0, 0, 0, 0.25)`,
-  }
+  },
 };
 
 export const Theme: FC = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
