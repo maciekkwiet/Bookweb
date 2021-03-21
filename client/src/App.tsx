906f57a60f827a193415ebab5d83b26522777512
wojaneta <wojaneta@gmail.com>
2021-03-21 21:22:13 +0100

    App.tsx fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,9 @@
 import { Theme } from './Theme';
-import { Button } from './Components/Button/Button';
-import { TextCard } from './Components/TextCard/TextCard';
 import { AppRoutes } from './routing/AppRoutes';
-
 
 function App() {
   return (
     <Theme>
-      <Button>Button</Button>
-      <TextCard authorName="authorName" authorLastName="authorLastName" description="description"></TextCard>
       <AppRoutes />
     </Theme>
   );
