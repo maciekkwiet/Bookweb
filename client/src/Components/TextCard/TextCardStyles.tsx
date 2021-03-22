33f480e0e82850a2c4e65f76477867e08c32dd54
wojaneta <wojaneta@gmail.com>
2021-03-22 18:31:32 +0100

    TextCard style changed

--- client/src/Components/TextCard/TextCardStyles.tsx
+++ client/src/Components/TextCard/TextCardStyles.tsx
@@ -3,7 +3,7 @@ import { theme } from '../../Theme';
 
 export const TextCardComponent = styled.div`
   width: 755px;
-  height: 180px;
+  height: auto;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-color: ${(props) => props.theme.colors.primary};
   border-radius: 10px;
@@ -11,17 +11,16 @@ export const TextCardComponent = styled.div`
   text-align: justify;
   padding: 20px;
   color: ${(props) => props.theme.colors.secondary};
+  font-family: ${(props) => props.theme.fonts.default};
 `;
 
 export const AuthorName = styled.h1`
-  font-family: ${(props) => props.theme.fonts.default};
   font-size: 15px;
   line-height: 16px;
   text-transform: uppercase;
 `;
 
 export const Description = styled.p`
-  font-family: ${(props) => props.theme.fonts.default};
   font-size: 14px;
   line-height: 17px;
 `;
