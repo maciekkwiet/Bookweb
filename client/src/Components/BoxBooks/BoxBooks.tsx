94a533a9d06169695a76403b25755ed2130cec90
Wojtek Warmuz <wwarmuz@gmail.com>
2021-03-24 19:33:51 +0100

    Add constants file

--- client/src/Components/BoxBooks/BoxBooks.tsx
+++ client/src/Components/BoxBooks/BoxBooks.tsx
@@ -1,18 +1,15 @@
-import { FC } from 'react';
+import { BoxBooksContent } from '../../constants';
 import { BoxBooksComponent, BoxBooksTitle, BoxBooksParagraph, BoxBooksImage } from './BoxBooksStyles';
 
 export const BoxBooks = () => {
   return (
     <BoxBooksComponent data-testid="BoxBooksId">
-      <BoxBooksTitle>
-        To tutaj:
-      </BoxBooksTitle>
-      <BoxBooksParagraph>- Dowiesz się co warto przeczytać</BoxBooksParagraph>
-      <BoxBooksParagraph>- Zaplanujesz najbliższą przygodę z książką</BoxBooksParagraph>
-      <BoxBooksParagraph>- Stworzysz kolekcję swoich ulubionych tytułów
-</BoxBooksParagraph>
-      <BoxBooksParagraph>- Ocenisz przeczytane książki</BoxBooksParagraph>
-      <BoxBooksParagraph>- Zaprosisz znajomych i dowiesz się co czytają</BoxBooksParagraph>
+      <BoxBooksTitle>{BoxBooksContent[0]}</BoxBooksTitle>
+      <BoxBooksParagraph>{BoxBooksContent[1]}</BoxBooksParagraph>
+      <BoxBooksParagraph>{BoxBooksContent[2]}</BoxBooksParagraph>
+      <BoxBooksParagraph>{BoxBooksContent[3]}</BoxBooksParagraph>
+      <BoxBooksParagraph>{BoxBooksContent[4]}</BoxBooksParagraph>
+      <BoxBooksParagraph>{BoxBooksContent[5]}</BoxBooksParagraph>
       <BoxBooksImage src={process.env.PUBLIC_URL + '/books.png'}></BoxBooksImage>
     </BoxBooksComponent>
   );
