eeab5d2c97a766d330de118c904d62da434348c6
wojaneta <wojaneta@gmail.com>
2021-03-27 18:08:14 +0100

    Avatar size changed

--- client/src/Components/Avatar/AvatarStyles.tsx
+++ client/src/Components/Avatar/AvatarStyles.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const AvatarComponent = styled.img`
-  width: 50px;
-  height: 50px;
+  width: 65px;
+  height: 65px;
   border-radius: 50%;
   box-shadow: 0 0 5px #505050;
 `;
