9c1122b1761d3165d90af5c9aa7a51c3e23e2844
wojaneta <wojaneta@gmail.com>
2021-03-27 13:41:10 +0100

    Header display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Theme } from './Theme';
 import { AppRoutes } from './routing/AppRoutes';
+import { Header } from './Components/Header/Header';
 
 function App() {
   return (
     <Theme>
       <AppRoutes />
+      <Header />
     </Theme>
   );
 }
