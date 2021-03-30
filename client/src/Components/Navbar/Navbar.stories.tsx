5c4bfb6b2183a821336677afa002c9acfc591905
Kamil Dzierzak <kamdzierzak@gmail.com>
2021-03-30 10:32:04 +0200

    whatislove

--- client/src/Components/Navbar/Navbar.stories.tsx
+++ client/src/Components/Navbar/Navbar.stories.tsx
@@ -1,1 +1,19 @@
-export {};
+import { Story } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { Navbar } from '../Navbar/Navbar';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Navbar',
+  component: Navbar,
+  decorators: [
+    (Story: any) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+};
+
+export const PrimaryNavbar: Story = () => <Navbar />;
