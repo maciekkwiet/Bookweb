4f2a449acda9359ba568d799483a2bbc7ace55d4
KlaraGajaszek <klaragajaszek21@wp.pl>
2021-04-01 00:55:25 +0200

    change formik to useForm

--- client/src/Components/RegistrationForm/RegistrationForm.tsx
+++ client/src/Components/RegistrationForm/RegistrationForm.tsx
@@ -1,30 +1,21 @@
-import { Formik, Form, FormikHelpers } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
+import { useFormik } from 'formik';
 import {
   RegistrationFormContainer,
   RegistrationInput,
   RegistrationButton,
   RegistrationFormTitle,
-  RegistrationErrorMessage
+  RegistrationErrorMessage,
 } from './RegistrationFormStyles';
-
+import * as Yup from 'yup';
 
 interface Values {
-  name: string;
   email: string;
   password: string;
   confirmPassword: string;
 }
 const SignupSchema = Yup.object().shape({
-  name: Yup.string()
-    .required('Nazwa użytkownika jest wymagana!'),
-  email: Yup.string()
-    .email("Niewłaściwy adres email")
-    .required("Email jest wymagany!"),
-  password: Yup.string()
-    .min(8, 'Hasło musi mieś conajmniej 8 znaków!')
-    .required('Hasło jest wymagane!'),
+  email: Yup.string().email('Niewłaściwy adres email').required('Email jest wymagany!'),
+  password: Yup.string().min(8, 'Hasło musi mieś conajmniej 8 znaków!').required('Hasło jest wymagane!'),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password'), ''], 'Hasło musi się zgadzać!')
     .required('Powtórzenie hasła jest wyamgane!'),
@@ -39,75 +30,64 @@ export const RegistrationForm = ({ onSubmit }: RegistrationFormType) => {
     onSubmit(values);
   };
 
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+    validationSchema: SignupSchema,
+  });
+
   return (
     <RegistrationFormContainer>
       <RegistrationFormTitle>Nie masz jeszcze konta?</RegistrationFormTitle>
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          password: '',
-          confirmPassword: '',
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={handleSubmit}
 
-      >
-        {({ errors, touched, values, handleChange, handleSubmit }) => (
-          <Form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
+        <RegistrationInput
+          id="email"
+          name="email"
+          placeholder="podaj email"
+          type="email"
+          onChange={formik.handleChange}
+          data-testid="inputId"
+          value={formik.values.email}
+        />
+        <RegistrationErrorMessage>
+          {formik.errors.email && formik.touched.email ? formik.errors.email : null}
+        </RegistrationErrorMessage>
 
-            <RegistrationInput
-              id="name"
-              name="name"
-              placeholder="podaj nazwę użytkownika"
-              type="name"
-              onChange={handleChange}
-              data-testid="inputId"
-              value={values.name} />
-            <RegistrationErrorMessage>{errors.name && touched.name ? errors.name : null}
-            </RegistrationErrorMessage>
+        <RegistrationInput
+          id="password"
+          name="password"
+          placeholder="wpisz hasło"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <RegistrationErrorMessage>
+          {formik.errors.password && formik.touched.password ? formik.errors.password : null}
+        </RegistrationErrorMessage>
 
-            <RegistrationInput
-              id="email"
-              name="email"
-              placeholder="podaj adres email"
-              type="email"
-              onChange={handleChange}
-              data-testid="inputId"
-              value={values.email}
-            />
-            <RegistrationErrorMessage>{errors.email && touched.email ? errors.email : null}</RegistrationErrorMessage>
+        <RegistrationInput
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          placeholder="powtórz hasło"
+          onChange={formik.handleChange}
+          value={formik.values.confirmPassword}
+        />
+        <RegistrationErrorMessage>
+          {formik.errors.confirmPassword && formik.touched.confirmPassword ? formik.errors.confirmPassword : null}
+        </RegistrationErrorMessage>
 
-            <RegistrationInput
-              id="password"
-              name="password"
-              placeholder="wpisz hasło"
-              type="password"
-              onChange={handleChange}
-              value={values.password}
-            />
-            <RegistrationErrorMessage>
-              {errors.password && touched.password ? errors.password : null}
-            </RegistrationErrorMessage>
-
-            <RegistrationInput
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              placeholder="powtórz hasło"
-              onChange={handleChange}
-              value={values.confirmPassword}
-            />
-            <RegistrationErrorMessage>
-              {errors.confirmPassword && touched.confirmPassword ? errors.confirmPassword : null}
-            </RegistrationErrorMessage>
-
-            <RegistrationButton type="submit" data-testid="buttonId">
-              Zarejestruj
-            </RegistrationButton>
-          </Form>
-        )}
-      </Formik>
+        <RegistrationButton type="submit" data-testid="buttonId">
+          Zarejestruj
+        </RegistrationButton>
+      </form>
     </RegistrationFormContainer>
   );
 };
@@ -131,27 +111,3 @@ export const RegistrationForm = ({ onSubmit }: RegistrationFormType) => {
 //     });
 //   setSubmitting(false);
 // }
-
-
-
-
-// onSubmit={(
-//   values: Values,
-//   { setSubmitting }: FormikHelpers<Values>
-// ) => {
-//   setSubmitting(true);
-//   const { name, email, password } = values;
-//   axios.post('http://localhost:8080/api/users/register', {
-//     name,
-//     email,
-//     password,
-//   })
-//     .then(res => {
-//       alert(res.data)
-//     }, (error) => {
-//       alert(error);
-//     })
-
-//   setSubmitting(false);
-
-// }}
