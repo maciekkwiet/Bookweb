a6e1809f30ff639d2aa7d67776849bc43305aa70
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-04-03 00:01:58 +0200

    Test in main file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,14 @@ import { AppRoutes } from './routing/AppRoutes';
 import { Provider } from 'react-redux';
 import store from './app/store';
 
-
 function App() {
   return (
-    <Provider store={store}>
-      <Theme>
-        <AppRoutes />
-      </Theme>
-    </Provider>
+    <h1>TEST JS</h1>
+    // <Provider store={store}>
+    //   <Theme>
+    //     <AppRoutes />
+    //   </Theme>
+    // </Provider>
   );
 }
 
