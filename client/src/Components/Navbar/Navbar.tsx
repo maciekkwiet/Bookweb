2b007e5513dd4025df4b81b11ff991f1ea448231
Kamil Dzierzak <kamdzierzak@gmail.com>
2021-04-03 18:53:28 +0200

    add a few tests

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -15,22 +15,26 @@ export const Navbar = () => {
     <IconContext.Provider value={{ color: theme.colors.secondary }}>
       <NavbarComponent>
         <NavContainer>
-          <Burger onClick={handleClick}>{click ? <FaTimes /> : <FaBars />}</Burger>
-          <NavMenu onClick={handleClick} click={click}>
+          <Burger onClick={handleClick} data-testid="BurgerId">
+            {click ? <FaTimes data-testid="CloseIconId" /> : <FaBars data-testid="BurgerIconId" />}
+          </Burger>
+          <NavMenu onClick={handleClick} click={click} data-testid="NavMenuId">
             <NavItem>
-              <NavLinks to="/catalog">Katalog książek</NavLinks>
+              <NavLinks to="/bookscatalog">Katalog książek</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="/add">Dodaj książkę</NavLinks>
+              <NavLinks to="/addnewbook">Dodaj książkę</NavLinks>
             </NavItem>
             <NavItem>
               <NavLinks to="/mybooks">Moje książki</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="/top">Top 100</NavLinks>
+              <NavLinks to="/ranking" data-testid="RankingLinkId">
+                Top 100
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="/providers">Katalog księgarń i bibliotek</NavLinks>
+              <NavLinks to="/providerscatalog">Katalog księgarń i bibliotek</NavLinks>
             </NavItem>
           </NavMenu>
         </NavContainer>
--- client/src/Components/Navbar/_tests_/Navbar.test.tsx
+++ client/src/Components/Navbar/_tests_/Navbar.test.tsx
@@ -1,5 +1,8 @@
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 import { Navbar } from '../Navbar';
 
@@ -10,5 +13,48 @@ describe('Navbar', () => {
         <Navbar />
       </MemoryRouter>,
     );
+  });
+
+  it('redirects to ranking page', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Navbar />
+      </Router>,
+    );
+    const rankingLink = screen.queryByTestId('RankingLinkId');
+    if (rankingLink) userEvent.click(rankingLink);
+    expect(history.location.pathname).toBe('/ranking');
+  });
+
+  it('changes the icon after click', () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>,
+    );
+    const burger = screen.queryByTestId('BurgerId');
+
+    const burgerIcon = screen.queryByTestId('BurgerIconId');
+    expect(burger?.children).toContain(burgerIcon);
+
+    if (burger) userEvent.click(burger);
+
+    const closeIcon = screen.queryByTestId('CloseIconId');
+    expect(burger?.children).toContain(closeIcon);
+  });
+
+  it('called function atfer click on burger', () => {
+    const mockCallback = jest.fn();
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>,
+    );
+
+    const burger = screen.queryByTestId('BurgerId');
+    if (burger) burger.onclick = mockCallback;
+    if (burger) userEvent.click(burger);
+    expect(mockCallback).toHaveBeenCalled();
   });
 });
