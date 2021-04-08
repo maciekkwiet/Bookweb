f656799bb979f45052326e280ead97708ccaaf13
wojaneta <wojaneta@gmail.com>
2021-04-08 22:07:45 +0200

    Comments eleted

--- client/src/Components/UserAcitivityBox/UserActivityBox.tsx
+++ client/src/Components/UserAcitivityBox/UserActivityBox.tsx
@@ -1,14 +1,5 @@
 import { FC } from 'react';
-// import {
-//   BoxComponent,
-//   ImgContainer,
-//   ContentContainer,
-//   BookParagraph,
-//   BookTitleParagraph,
-//   BoxBookReview,
-//   BoxReadMore,
-//   BookImage,
-// } from './UserAcitivityBoxStyles';
+
 import { Box } from '../Box/Box';
 import StarRating from '../Star/StarRating';
 import { ActivityTitle } from './UserAcitivityBoxStyles';
@@ -23,29 +14,6 @@ export type UserActivityBoxProps = {
   image: string;
   username: string;
 };
-
-// export const Box: FC<BoxComponentProps> = ({ title, author, rate, review, image }) => {
-//   const readMoreHandler = () => {};
-
-//   return (
-//     <BoxComponent data-testid="BoxId">
-//       <ImgContainer>
-//         <BookImage src={image}></BookImage>
-//       </ImgContainer>
-//       <ContentContainer>
-//         <BookTitleParagraph>{title}</BookTitleParagraph>
-//         <BookParagraph>{author}</BookParagraph>
-//         <BookParagraph>Średnia ocena: {rate}</BookParagraph>
-//         <BoxBookReview>
-//           {review}
-//           <BoxReadMore onClick={readMoreHandler} data-testid="BoxReadMoreId">
-//             czytaj więcej
-//           </BoxReadMore>
-//         </BoxBookReview>
-//       </ContentContainer>
-//     </BoxComponent>
-//   );
-// };
 
 export const UserActivityBox: FC<UserActivityBoxProps> = ({ title, author, rate, review, image, username }) => {
   const readMoreHandler = () => {};
