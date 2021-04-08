868d7c2ef285ac19eb0bbef24ab8f3222102107a
wojaneta <wojaneta@gmail.com>
2021-04-08 22:05:01 +0200

    UserActivity component style change (for smaller screens)

--- client/src/Components/UserAcitivityBox/UserAcitivityBoxStyles.tsx
+++ client/src/Components/UserAcitivityBox/UserAcitivityBoxStyles.tsx
@@ -6,47 +6,31 @@ export const ActivityBox = styled(TextCardComponent)`
   display: flex;
   flex-direction: column;
   width: 32%;
+  @media only screen and (max-width: ${(props) => props.theme.size.sm}) {
+    width: 35%;
+    height: auto;
+  } ;
 `;
 export const RatingDiv = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 1%;
+  @media only screen and (max-width: ${(props) => props.theme.size.sm}) {
+    padding: 0;
+    flex-direction: column;
+  }
+  @media only screen and (max-width: ${(props) => props.theme.size.lg}) {
+    flex-direction: column;
+  } ;
 `;
 
-// export const BookImage = styled.img`
-//   width: 100%;
-//   height: 100%;
-// `;
-
-// export const ContentContainer = styled.div`
-//   box-sizing: border-box;
-//   padding: 1%;
-//   flex-basis: 75%;
-//   display: flex;
-//   flex-direction: column;
-// `;
-// export const BookParagraph = styled.p`
-//   margin: 5px 0 0 0;
-//   text-align: left;
-// `;
 export const ActivityTitle = styled.span`
   margin: 2px 0 0 0;
   text-align: left;
   font-weight: bold;
   font-size: 18px;
   color: black;
-  // align-self: flex-start;
+  @media only screen and (max-width: ${(props) => props.theme.size.sm}) {
+    font-size: 12px;
+  } ;
 `;
-// export const BoxBookReview = styled.p`
-//   margin: 5px 0 0 0;
-//   text-align: justify;
-//   font-size: 13px;
-// `;
-// export const BoxReadMore = styled.a`
-//   display: inline-block;
-//   color: ${(props) => props.theme.colors.readMore};
-//   text-align: left;
-//   font-size: 14px;
-//   max-width: 90%;
-//   cursor: pointer;
-// `;
