83d0dcf1e1a24f2c253fd2876f222a00a2da1e33
wojaneta <wojaneta@gmail.com>
2021-04-10 11:28:34 +0200

    Theme background color added

--- client/src/Theme.tsx
+++ client/src/Theme.tsx
@@ -17,6 +17,7 @@ export const theme = {
     darkTransparent: `rgba(0,0,0,0.15)`,
     mainGradient: `linear-gradient(312.01deg, #CB997E 0.66%, #9EB2CD 100%)`,
     bigCardGradient: `linear-gradient(289.63deg, #B98163 8.13%, rgba(70, 68, 66, 0) 139.61%);`,
+    headerAndFooter: `#F2F4F3`,
   },
   fonts: {
     default: `Montserrat`,
