48d9389464ec01c263d278236c29e31303b6bb55
wojaneta <wojaneta@gmail.com>
2021-04-10 15:52:33 +0200

    Endpoints for TopBooks

--- api/routes/books/bookRoutes.ts
+++ api/routes/books/bookRoutes.ts
@@ -1,8 +1,9 @@
 const Router = require('express-promise-router');
-import { getBooks, getBookById, createBook, updateBook, deleteBook } from './book';
+import { getBooks, getBookById, createBook, updateBook, deleteBook, getTopBooks } from './book';
 
 const router = new Router();
 router.get('/', getBooks);
+router.get('/top', getTopBooks);
 router.get('/:id', getBookById);
 router.post('/', createBook);
 router.put('/:id', updateBook);
