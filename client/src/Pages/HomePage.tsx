eaee783bdfd37b962e77ea99d33d16e4f6edec79
wojaneta <wojaneta@gmail.com>
2021-04-11 17:09:06 +0200

    HomePage components updated

--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 
 import { BoxBooks } from '../Components/BoxBooks/BoxBooks';
-import ChooseGenre from '../Components/ChooseGenre/ChooseGenre';
+// import ChooseGenre from '../Components/ChooseGenre/ChooseGenre';
 import { Header } from '../Components/Header/Header';
 import { Footer } from '../Components/Footer/Footer';
 import { TopBooksBox } from '../Components/TopBooks/TopBooksComponent';
@@ -11,6 +11,7 @@ import { RegistrationForm } from '../Components/RegistrationForm/RegistrationFor
 import { useEffect, useState } from 'react';
 import { BookDetails as BookDetailsType } from '../Components/TopBooks/TopBooksComponent';
 import { Axios } from '../helpers/axios';
+import { TitleDiv } from '../Components/TopBooks/TopBookComponentStyles';
 // import { selectUser } from '../Features/User/UserSlice';
 
 export const HomePage = () => {
@@ -39,7 +40,7 @@ export const HomePage = () => {
           alignItems: 'center',
           padding: '5em 0em',
           flexWrap: 'wrap',
-          marginBottom: '50px',
+          // marginBottom: '50px',
           background: 'linear-gradient(312.01deg, #CB997E 0.66%, #9EB2CD 100%)',
           width: '100%',
         }}
@@ -48,8 +49,10 @@ export const HomePage = () => {
         <RegistrationForm onSubmit={() => {}} />
       </div>
       <TopBooksDiv>
-        <TopBooksTitle>Najlepiej oceniane</TopBooksTitle>
-        <ChooseGenre />
+        <TitleDiv>
+          <TopBooksTitle>Najlepiej oceniane</TopBooksTitle>
+        </TitleDiv>
+        {/* <ChooseGenre /> */}
         <TopBooksBox topBooks={topBooks}> </TopBooksBox>
         {/* <ChooseGenre /> */}
         {/* <TopBooksBox topBooks={sampleBooks}> </TopBooksBox> */}
