29a1d2cd5807c27033bf629e3b6944e8ca7ded93
wojaneta <wojaneta@gmail.com>
2021-04-11 20:36:50 +0200

    Logging added

--- client/src/Components/Header/AnonymousDiv.tsx
+++ client/src/Components/Header/AnonymousDiv.tsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
+import { Axios } from '../../helpers/axios';
 import { LogInput, LogDiv, LogButton } from './AnonymousDivStyles';
 
 export const AnonymousDiv = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = async (source) => {
+    const { data, status } = await Axios.post('/api/users/login', { email, password });
+
+    if (status === 200) {
+      localStorage.setItem('token', data.token);
+    }
+  };
+
   return (
     <LogDiv>
-      <LogInput type={'email'} placeholder={'adres e-mail'} required={true} />
-      <LogInput type={'password'} placeholder={'hasło'} required={true} />
-      <LogButton>ZALOGUJ</LogButton>
+      <LogInput
+        type={'email'}
+        placeholder={'adres e-mail'}
+        required={true}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <LogInput
+        type={'password'}
+        placeholder={'hasło'}
+        required={true}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <LogButton onClick={login}>ZALOGUJ</LogButton>
     </LogDiv>
   );
 };
