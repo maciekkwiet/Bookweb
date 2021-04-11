6ba58ae19d33f048c7744168959e4090b0693f86
wojaneta <wojaneta@gmail.com>
2021-04-11 20:34:54 +0200

    Authors and reviews added to TopBooks

--- client/src/Components/TopBooks/TopBooksComponent.tsx
+++ client/src/Components/TopBooks/TopBooksComponent.tsx
@@ -6,9 +6,9 @@ import { TopBooksComponent } from './TopBookComponentStyles';
 export type BookDetails = {
   id: number;
   title: string;
-  // author: string;
+  author: string;
   rating: string;
-  // review: string;
+  review: string;
   cover: string;
 };
 
@@ -17,29 +17,16 @@ export type TopBooksProps = {
 };
 
 export const TopBooksBox: FC<TopBooksProps> = ({ topBooks }) => {
-  // const [topBooks, setTopBooks] = useState<BookDetailsType[]>([])
-
-  // useEffect(() => {
-  //     const fetch = async () => {
-  //       const { data } = await Axios.get('/api/books/top');
-
-  //       setTopBooks(data);
-  //     };
-
-  //     fetch();
-  // });
-
-  console.log(topBooks);
-
   const booksList = topBooks.map((e) => (
     <Box
       title={e.title}
-      author={'wczytywanie...'}
-      review={'wczytywanie...'}
+      author={e.author}
+      review={e.review}
       rate={parseFloat(e.rating)}
       image={e.cover}
       key={e.id}
     ></Box>
   ));
+
   return <TopBooksComponent>{booksList}</TopBooksComponent>;
 };
