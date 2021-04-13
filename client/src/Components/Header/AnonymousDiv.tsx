58558d78fa65f3d6d107c5c1ce9e736601a378c4
wojaneta <wojaneta@gmail.com>
2021-04-13 22:01:58 +0200

    Error Messages added

--- client/src/Components/Header/AnonymousDiv.tsx
+++ client/src/Components/Header/AnonymousDiv.tsx
@@ -1,36 +1,67 @@
 import { useState } from 'react';
 import { Axios } from '../../helpers/axios';
-import { LogInput, LogDiv, LogButton } from './AnonymousDivStyles';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useToasts } from 'react-toast-notifications';
+
+import { LogInput, LogDiv, LogButton, ErrorMessageEmail, ErrorMessagePassword } from './AnonymousDivStyles';
+
+interface Values {
+  email: string;
+  password: string;
+}
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string().email('Niewłaściwy adres email').required('Email jest wymagany!'),
+  password: Yup.string().min(8, 'Hasło musi zawierać conajmniej 8 znaków!').required('Hasło jest wymagane!'),
+});
 
 export const AnonymousDiv = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const login = async (source) => {
-    const { data, status } = await Axios.post('/api/users/login', { email, password });
-
-    if (status === 200) {
-      localStorage.setItem('token', data.token);
-    }
-  };
+  const { addToast } = useToasts();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: async (values: Values) => {
+      try {
+        const { data } = await Axios.post('/api/users/login', { email: values.email, password: values.password }, {});
+        localStorage.setItem('token', data.token);
+      } catch (e) {
+        addToast(`Nieprawidłowa nazwa użytkownika lub hasło`, {
+          appearance: 'error',
+        });
+      }
+    },
+    validationSchema: SignupSchema,
+  });
 
   return (
     <LogDiv>
-      <LogInput
-        type={'email'}
-        placeholder={'adres e-mail'}
-        required={true}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <LogInput
-        type={'password'}
-        placeholder={'hasło'}
-        required={true}
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <LogButton onClick={login}>ZALOGUJ</LogButton>
+      <form onSubmit={formik.handleSubmit}>
+        <LogInput
+          type={'email'}
+          name={'email'}
+          placeholder={'adres e-mail'}
+          required={true}
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <ErrorMessageEmail>{formik.errors.email && formik.touched.email ? formik.errors.email : ''}</ErrorMessageEmail>
+
+        <LogInput
+          type={'password'}
+          name={'password'}
+          placeholder={'hasło'}
+          required={true}
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <ErrorMessagePassword>
+          {formik.errors.password && formik.touched.password ? formik.errors.password : ''}
+        </ErrorMessagePassword>
+        <LogButton type="submit">ZALOGUJ</LogButton>
+      </form>
     </LogDiv>
   );
 };
