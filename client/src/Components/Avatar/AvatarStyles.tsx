0f97cce682a78442b58984900cb619edca3849a7
wojaneta <wojaneta@gmail.com>
2021-04-14 22:25:51 +0200

    Avatar styles fixed

--- client/src/Components/Avatar/AvatarStyles.tsx
+++ client/src/Components/Avatar/AvatarStyles.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const AvatarComponent = styled.img`
-  width: 100%;
-  height: 100%;
+  width: 50px;
+  height: 50px;
   border-radius: 50%;
   box-shadow: 0 0 5px #505050;
 `;
