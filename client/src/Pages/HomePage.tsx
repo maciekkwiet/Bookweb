b6f1e1f472df0df7ba68cefb25c63514099defbb
WarmuzWojtek <wwarmuz@gmail.com>
2021-04-15 01:43:35 +0200

    Add switch homepage

--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -1,9 +1,20 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+import { useState, useEffect } from 'react'
 import { RegisterHomePage } from './RegisterHomePage';
+import { LoggedHomePage } from './LoggedHomePage';
 
 export const HomePage = () => {
+
+  const user = useSelector((state: RootState) => state.user?.user);
+  const [isLogged, setIsLogged] = useState(false);
+  useEffect(() => {
+    user ? setIsLogged(true) : setIsLogged(false)
+  }, [user]);
+
   return (
-    <div>
-      <RegisterHomePage></RegisterHomePage>
-    </div>
+    <>
+      {isLogged ? <LoggedHomePage /> : <RegisterHomePage />}
+    </>
   );
 };
