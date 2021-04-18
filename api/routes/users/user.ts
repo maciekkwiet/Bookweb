67104075407060399c7e0fc1b66c1be2acfde60a
Sebastian <sebek1995hleb@gmail.com>
2021-04-18 21:56:00 +0200

    User endpoints

--- api/routes/users/user.ts
+++ api/routes/users/user.ts
@@ -1,127 +1,116 @@
 import { Request, Response } from 'express';
-import pool from '../../configDB/config';
-const bcrypt = require('bcrypt');
-const jwtGenerator = require('../../utils/jwtGenerator');
+
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+
+import pool from '../../configDB/config';
+import validateEntity from '../../helpers/validateEntity';
 import { uploadImage } from '../../utils/imageTools';
-import { initialShelves } from '../../utils/constants';
+import { User } from '../../models';
+const jwtGenerator = require('../../utils/jwtGenerator');
 
-//Funkcja którą można by wynieść i używać do wyszukiwania po ID - nie wiem jak z obsługą błędów - może za pomocą try/catch
-const getById = async (table: string, id: number) => {
-  let rows: object[] = [];
-  await pool
-    .query(`SELECT * FROM ${table} WHERE ID = $1`, [id])
-    .then((results) => {
-      rows = results.rows;
-    })
-    .catch((error) => {
-      throw new Error(error);
-    });
+export const getAllUsers = async (request: Request, response: Response) => {
+  const users = await User.find({
+    select: ['id', 'name', 'email', 'avatar'],
+  });
 
-  return rows;
-};
-//
-
-//ROUTES
-
-export const getAllUsers = async (req: Request, res: Response) => {
-  await pool
-    .query('SELECT * FROM users ORDER BY id ASC')
-    .then((results) => res.status(200).json(results.rows))
-    .catch((error) => res.status(500).json(error.message));
+  return response.status(200).send(users);
 };
 
-export const getUser = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const user = await getById('users', id);
+export const getUser = async (request: Request, response: Response) => {
+  const id = request.params.id;
 
-  if (user.length === 0) return res.status(400).json("User with given ID doesn't exist");
+  const user = await User.findOneOrFail({
+    where: { id },
+    relations: ['books', 'reviews'],
+    select: ['id', 'name', 'email', 'avatar'],
+  });
 
-  res.status(200).json(user);
-};
-
-export const removeUser = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const user = await getById('users', id);
-
-  if (user.length === 0) return res.status(400).json("User with given ID doesn't exist");
-
-  pool.query('DELETE FROM users where id = $1', [id]).then(() => {
-    res.status(200).json({
-      message: 'User deleted',
-      user,
-    });
+  return response.status(200).send({
+    user,
   });
 };
 
-export const updateUser = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const { email, password, name, surname, avatar } = req.body;
-  const avatarImage = await uploadImage(avatar);
+export const removeUser = async (request: Request, response: Response) => {
+  const id = request.params.id;
+  const user = await User.findOneOrFail(id);
 
-  let user = await getById('users', id);
-  if (user.length === 0) return res.status(400).json("User with given ID doesn't exist");
+  await user.remove();
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  return response.status(200).send({
+    message: `User with email: ${user.email} has been deleted`,
+  });
+};
 
-  pool
-    .query('UPDATE users SET email = $1, password = $2, name = $3, surname = $4, avatar = $5 WHERE id = $6', [
-      email,
-      hashedPassword,
-      name,
-      surname,
-      avatarImage,
-      id,
-    ])
-    .then(async () => {
-      user = await getById('users', id);
-      res.status(200).json({ message: 'User updated', user });
-    });
+export const updateUser = async (request: Request, response: Response) => {
+  const id = request.params.id;
+  const { name, email, password, avatar } = request.body;
+
+  const user = await User.findOneOrFail(id);
+
+  if (name) user.name = name;
+  if (email) user.email = email;
+  if (avatar) {
+    const avatarImage = await uploadImage(avatar);
+    user.avatar = avatarImage;
+  }
+  if (password) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    user.password = hashedPassword;
+  }
+
+  await validateEntity(user);
+
+  await user.save();
+
+  //user zwracany wraz z hasłem w celach testowych - do usunięcia
+  return response.status(200).send({
+    message: 'User updated',
+    user: user,
+  });
 };
 
 export const registerUser = async (req: Request, res: Response) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(422).json({ errors: errors.array() });
-      return;
-    }
-    //1. destructure the req.body (name, email, password)
-    const { name, email, password } = req.body;
-
-    //2. check if user exist (if user exist then throw error)
-    const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-    if (user.rows.length !== 0) {
-      return res.status(401).send('User already exist');
-    }
-
-    //3. Bcrypt the user password
-    const salt = await bcrypt.genSalt(10);
-    const bcryptPassword = await bcrypt.hash(password, salt);
-
-    //4. enter the new user inside our db
-
-    const newUser = await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [
-      name,
-      email,
-      bcryptPassword,
-    ]);
-
-
-    initialShelves.map(async (s: string) => {
-      await pool.query('INSERT INTO book_groups (name, user_id) VALUES ($1, $2)', [s, newUser.rows[0].id]);
-    });
-
-    newUser.rows[0].token = jwtGenerator(newUser.rows[0].id);
-
-    res.json(newUser.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return;
   }
+
+  const { name, email, password } = req.body;
+  const user = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+
+  if (user) {
+    return res.status(401).send('User already exist');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const bcryptPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User();
+  newUser.name = name;
+  newUser.email = email;
+  newUser.password = bcryptPassword;
+
+  await newUser.save();
+
+  const filter = (user: User) => {
+    const { name, email } = newUser;
+    return { name, email };
+  };
+
+  res.status(201).send({
+    message: 'User created',
+    user: filter(newUser),
+  });
 };
 
+//todo
 export const loginUser = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req);
