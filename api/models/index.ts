7a3ad0fa94e2fa3f0b6bdf7d0c196324c79e0e4f
Sebastian <sebek1995hleb@gmail.com>
2021-04-18 15:14:49 +0200

    Export entities

---
+++ api/models/index.ts
@@ -0,0 +1,7 @@
+import { User } from './User.entity';
+import { Review } from './Review.entity';
+import { Book } from './Book.entity';
+import { Author } from './Author.entity';
+import { Category } from './Category.entity';
+
+export { User, Book, Review, Author, Category };
