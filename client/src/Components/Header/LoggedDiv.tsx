beedd77848d920d5c63b510b50ec30c14fb52e97
WarmuzWojtek <wwarmuz@gmail.com>
2021-04-20 06:53:03 +0200

    DIsplay LoggedUser

--- client/src/Components/Header/LoggedDiv.tsx
+++ client/src/Components/Header/LoggedDiv.tsx
@@ -1,11 +1,14 @@
 import { LogDiv, UserName } from './LoggedDivStyles';
 import { Avatar } from '../Avatar/Avatar';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
 
 export const LoggedDiv = ({ userID, userName }) => {
+  const loggedUser = useSelector((state: RootState) => state.user?.user);
   return (
     <LogDiv>
       <Avatar />
-      <UserName>{userName}</UserName>
+      <UserName>{loggedUser?.name}</UserName>
     </LogDiv>
   );
 };
