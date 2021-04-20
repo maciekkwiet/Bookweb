af33a46eba3cc29a7d8d7d3fd8b2ffecdfbcda4b
Kamil Dzierzak <kamdzierzak@gmail.com>
2021-04-20 10:09:01 +0200

    add closing modal when outside click

--- client/src/Components/Modal/Modal.tsx
+++ client/src/Components/Modal/Modal.tsx
@@ -1,10 +1,19 @@
+import { useRef } from 'react';
 import { Background, CloseModalButton, ModalWrapper } from './ModalStyles';
 
 export const Modal = ({ showModal, setShowModal, children }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
   return (
     <>
       {showModal ? (
-        <Background>
+        <Background ref={modalRef} onClick={closeModal}>
           <ModalWrapper>
             <CloseModalButton aria-label="Close modal" onClick={() => setShowModal((prev) => !prev)} />
             {children}
